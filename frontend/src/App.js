c5a07288a97ff5a36ab39e5a4549776aeb075c64
Utkarsh02121998 <utkarshuk02@gmail.com>
2022-04-13 00:08:31 +0530

    partial changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,9 @@ import Footer from './componenets/Footer/Footer';
 import Home from './routes/Home/Home';
 import Product from './routes/Product/Product';
 import Cart from './routes/Cart/Cart';
+import Login from './components/Login/Login.js'
+import Register from './components/Register/Register.js'
+import ForgotPassword from './components/ForgotPassword/ForgotPassword.js'
 
 
 function App() {
@@ -21,12 +24,17 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />}/>
             
-            
             <Route path="/shop" element={<Shop /> }/>
 
             <Route path="/product" element={<Product /> }/>
             
             <Route path="/cart" element={<Cart /> }/>
+
+            <Route path="/password/forgot" element={<ForgotPassword/>}/>
+
+            <Route path='/login' element={<Login/>}/>
+
+            <Route path='/register' element={<Register/>}/>
           </Routes>
           <Footer />
       </BrowserRouter>
