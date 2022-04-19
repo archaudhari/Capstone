a3cbd3f75ade27cb4bce34187c62737f20164918
tejaspatil746 <tejaspatil746@gmail.com>
2022-04-19 12:50:46 +0530

    added reviews features

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,14 +1,21 @@
 const express =require("express");
-const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews } = require("../controllers/productController");
+const { deleteReview } = require("../controllers/shopController");
 const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.route("/products").get( getAllProducts);
-router.route("/product/new").post(isAuthenticatedUser,authorizeRoles("seller"),createProduct);
-router.route("/product/:id")
+
+router.route("/seller/product/new").post(isAuthenticatedUser,authorizeRoles("seller"),createProduct);
+router.route("/seller/product/:id")
 .put(isAuthenticatedUser,authorizeRoles("seller"),updateProduct)
-.delete(isAuthenticatedUser,authorizeRoles("seller"),deleteProduct)
-.get(getProductDetails);
+.delete(isAuthenticatedUser,authorizeRoles("seller"),deleteProduct);
+
+router.route("/product/:id").get(getProductDetails);
+
+router.route("/review").put(isAuthenticatedUser, createProductReview);
+
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser,deleteReview);
 
 module.exports = router
--- backend/routes/shopRoute.js
+++ backend/routes/shopRoute.js
@@ -1,14 +1,21 @@
 const express =require("express");
-const { getAllShops, createShop, updateShop, deleteShop, getShopDetails } = require("../controllers/shopController");
+const { getAllShops, createShop, updateShop, deleteShop, getShopDetails, createShopReview, getShopReviews, deleteReview } = require("../controllers/shopController");
 const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/shops").get(getAllShops);
-router.route("/shop/new").post(isAuthenticatedUser,authorizeRoles("admin"), createShop);
-router.route("/shop/:id")
+router.route("/admin/shop/new").post(isAuthenticatedUser,authorizeRoles("admin"), createShop);
+router.route("/admin/shop/:id")
 .put(isAuthenticatedUser, authorizeRoles("admin"),updateShop)
-.delete(isAuthenticatedUser,authorizeRoles("admin"), deleteShop)
-.get(getShopDetails);
+.delete(isAuthenticatedUser,authorizeRoles("admin"), deleteShop);
+
+router.route("/shop/:id").get(getShopDetails);
+
+
+router.route("/shop/review").put(isAuthenticatedUser, createShopReview);
+
+router.route("/shop/reviews").get(getShopReviews).delete(isAuthenticatedUser,deleteReview);
+
 
 
 module.exports = router
